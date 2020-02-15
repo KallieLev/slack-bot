58ee158681703fb3a0a9dd52393823b88dc1f98c
KallieLevy <kali.levy1@gmail.com>
2020-02-15 13:20:47 +0200

    fixed unnecessary "try"

--- slack/send_message.py
+++ slack/send_message.py
@@ -3,6 +3,5 @@ import requests.exceptions
 
 
 def send_message_to_channel(url, token, channel, text):
-    try:
-        return requests.post(url=url,
-                             params={'token': token, 'channel': channel, 'text': text})
+    return requests.post(url=url,
+                         params={'token': token, 'channel': channel, 'text': text})
