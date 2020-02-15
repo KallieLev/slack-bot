df77331d643fc93c92b83975e4d2447714e04be1
KallieLevy <kali.levy1@gmail.com>
2020-02-15 13:20:19 +0200

    deleted except

--- slack/send_message.py
+++ slack/send_message.py
@@ -4,7 +4,5 @@ import requests.exceptions
 
 def send_message_to_channel(url, token, channel, text):
     try:
-        requests.post(url=url,
-                  params={'token': token, 'channel': channel, 'text': text})
-    except requests.exceptions as e:
-        print(e)
+        return requests.post(url=url,
+                             params={'token': token, 'channel': channel, 'text': text})
