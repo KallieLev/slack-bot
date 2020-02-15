698485e00d60a6878306b44bae989d8150bf591f
KallieLevy <kali.levy1@gmail.com>
2020-02-15 13:17:57 +0200

    added try and except to message sending

--- slack/send_message.py
+++ slack/send_message.py
@@ -1,6 +1,10 @@
 import requests
+import requests.exceptions
 
 
 def send_message_to_channel(url, token, channel, text):
-    requests.post(url=url,
+    try:
+        requests.post(url=url,
                   params={'token': token, 'channel': channel, 'text': text})
+    except requests.exceptions as e:
+        print(e)
