39aa8fbbf71409343ea4d48bc2cfacc5b38207f3
KallieLevy <kali.levy1@gmail.com>
2020-02-15 20:19:34 +0200

    added api endpoint to new content

--- slack_bot/__init__.py
+++ slack_bot/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from slack_bot.slack.send_message import send_message_to_channel
+from external_libs.slack.send_message import send_message_to_channel
 from flask_apscheduler import APScheduler
 import config
 import datetime
@@ -8,8 +8,10 @@ app = Flask(__name__)
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.add_job(id='send current time',
-                  func=lambda: send_message_to_channel(config.base_url + config.send_message_endpoint, config.token,
-                                                       config.every_hour_message_channel, str(datetime.datetime.now())),
+                  func=lambda: send_message_to_channel(config.base_url + config.send_message_endpoint,
+                                                       config.slack_token,
+                                                       config.every_hour_message_channel,
+                                                       str(datetime.datetime.now())),
                   trigger='interval', hours=config.hours_interval_of_now)
 scheduler.start()
 
--- slack_bot/views.py
+++ slack_bot/views.py
@@ -1,8 +1,23 @@
 import datetime
-from slack_bot import app
 from flask import jsonify
+
+from external_libs.twitter.get_tweets import get_python_content_last_hour
+from slack_bot import app
 
 
 @app.route('/now', methods=['POST'])
 def send_current_hour():
     return jsonify({'text': str(datetime.datetime.now())})
+
+
+@app.route('/new-content', methods=['POST'])
+def new_content_of_python_pages():
+    tweets = get_python_content_last_hour()
+    resp = []
+    for user, user_tweets in tweets.items():
+        if len(user_tweets) > 0:
+            resp.append(f'----the new tweets from {user}----\n')
+            resp.append('\n----------------\n'.join(user_tweets))
+    if not resp:
+        return jsonify({'text': 'sorry no new content'})
+    return jsonify({'text': '\n'.join(resp)})
